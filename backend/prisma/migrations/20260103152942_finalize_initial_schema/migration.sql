b4b15ad15355754a3127a08f3f807bbc951f1c03
Mehdux69 <mehdi.arf@live.fr>
2026-01-05 23:17:07 +0100

    Update migration.sql

--- backend/prisma/migrations/20260103152942_finalize_initial_schema/migration.sql
+++ backend/prisma/migrations/20260103152942_finalize_initial_schema/migration.sql
@@ -1,9 +1,3 @@
--- DropIndex
-DROP INDEX "AuditLog_correlationId_idx";
-
--- DropIndex
-DROP INDEX "AuditLog_tenantId_createdAt_idx";
-
 -- AlterTable
 ALTER TABLE "Document" ALTER COLUMN "textExtractedAt" SET DATA TYPE TIMESTAMP(3),
 ALTER COLUMN "ingestionQueuedAt" SET DATA TYPE TIMESTAMP(3);
